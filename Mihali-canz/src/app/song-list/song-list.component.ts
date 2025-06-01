530b9dafaa328d19011d2c10607f0b61ec221d4b
Seba-33 <159021649+Seba-33@users.noreply.github.com>
2025-06-01 15:02:41 +0000

    quarto punto

---
+++ Mihali-canz/src/app/song-list/song-list.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+import { Song } from '../Models/song.model';
+
+@Component({
+  selector: 'app-song-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './song-list.component.html',
+  styleUrl: './song-list.component.css'
+})
+export class SongListComponent {
+  @Input() canz! : Song
+}
